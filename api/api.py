d0ca6d870a060fb9d70ca7dedfa9afc4b20dbc08
Yashovardhan Thevalil <yashthevalil@gmail.com>
2021-12-08 14:48:03 +0100

    Added documentation

--- api/api.py
+++ api/api.py
@@ -5,12 +5,21 @@ from abc import abstractmethod
 
 class Api:
     def __init__(self, base_url: str):
+        """
+        Instantiate an API to fetch resources
+        :param base_url: The API base URL
+        """
         if base_url.endswith("/"):
             self.base_url = base_url[:-1]
         else:
             self.base_url = base_url
 
     async def fetch(self, path: str) -> dict:
+        """
+        Calls an API endpoint with a GET request
+        :param path: Path following the API base URL
+        :return: The JSON payload
+        """
         with urllib.request.urlopen(f"{self.base_url}{path}") as url:
             data = json.loads(url.read().decode(encoding="utf8"))
         return data
@@ -21,5 +30,10 @@ class Api:
 
 
 def dump_to_json(file_path: str, data: dict):
+    """
+    Dumps a Python dict to a JSON file
+    :param file_path: The output file path
+    :param data: The data to dump
+    """
     with open(file_path, "w", encoding="utf8") as f:
         json.dump(data, fp=f)
