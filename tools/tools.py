06967a6a0ba58cfad1e846910faf606a6806ef5a
Yashovardhan Thevalil <yashthevalil@gmail.com>
2021-12-09 19:25:36 +0100

    Improved decorator to use custom executor

--- tools/tools.py
+++ tools/tools.py
@@ -2,10 +2,13 @@ import asyncio
 import functools
 
 
-def run_in_executor(f):
-    @functools.wraps(f)
-    def inner(*args, **kwargs):
-        loop = asyncio.get_running_loop()
-        return loop.run_in_executor(None, lambda: f(*args, **kwargs))
+def run_in_executor(executor):
+    def function_wrapper(f):
+        @functools.wraps(f)
+        def task_to_run(*args, **kwargs):
+            loop = asyncio.get_running_loop()
+            return loop.run_in_executor(executor, lambda: f(*args, **kwargs))
 
-    return inner
+        return task_to_run
+
+    return function_wrapper
