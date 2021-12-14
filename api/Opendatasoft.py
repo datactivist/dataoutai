30d547c8efe55d501298a33070d133d0bdc02e5b
Elie Antoine <elie.antoine@etu.univ-amu.fr>
2021-12-14 20:41:19 +0100

    Change source for the fetching of the dataset and the schema of the datas fetched. A problem still persist : some datasets are broken so the whole batch fetched is lost if one have a problem.

--- api/Opendatasoft.py
+++ api/Opendatasoft.py
@@ -1,5 +1,6 @@
 import asyncio
 from typing import List
+from urllib.error import HTTPError
 
 from api import Api
 from tools import remove_xml_tags
@@ -7,7 +8,7 @@ from tools import remove_xml_tags
 
 class Opendatasoft(Api):
     def __init__(self):
-        super().__init__("https://public.opendatasoft.com/api/v2/catalog/")
+        super().__init__("https://data.opendatasoft.com/api/v2/catalog/")
 
     async def get_dataset_list(self, nb_per_query: int = 50) -> List[str]:
         """
@@ -24,6 +25,10 @@ class Opendatasoft(Api):
             for offset in range(0, count, nb_per_query)
         ]
 
+    def __check_frequency(self, frequency: str):
+        if frequency.startswith("http"):
+            return frequency.split("/")[-1]
+
     async def get_dataset_details(self, dataset_url: str) -> List[dict]:
         """
         Get the important dataset details:
@@ -34,39 +39,75 @@ class Opendatasoft(Api):
         :param dataset_url: The url query of the dataset to get
         :return: The dataset important details
         """
-        datasets = await self.fetch(dataset_url)
-        clean_data = []
-        for dataset in datasets["datasets"]:
-            clean_data.append(
-                {
-                    "dataset_name": f'{dataset["dataset"]["dataset_id"]}',
-                    "metadata": {
-                        "keywords": []
-                        if dataset["dataset"]["metas"]["default"]["keyword"] is None
-                        else [
-                            keyword
-                            for keyword in dataset["dataset"]["metas"]["default"][
-                                "keyword"
-                            ]
+        try:
+            datasets = await self.fetch(dataset_url)
+            for dataset in datasets["datasets"]:
+                if "dcat" not in dataset["dataset"]["metas"]:
+                    pass
+                else:
+                    print(
+                        self.__check_frequency(
+                            dataset["dataset"]["metas"]["dcat"]["accrualperiodicity"]
+                        )
+                    )
+            clean_data = []
+            for dataset in datasets["datasets"]:
+                clean_data.append(
+                    {
+                        "dataset_name": f'{dataset["dataset"]["dataset_id"]}',
+                        "maintainer": None
+                        if "custom" not in dataset["dataset"]["metas"]["default"]
+                        else dataset["dataset"]["metas"]["default"]["custom"][
+                            "gestionnaire"
                         ],
-                        "description": ""
-                        if dataset["dataset"]["metas"]["default"]["description"] is None
-                        else f'{remove_xml_tags(dataset["dataset"]["metas"]["default"]["description"])}',
-                    },
-                    "columns": [
-                        {"name": annotation["name"], "type": annotation["type"]}
-                        for annotation in dataset["dataset"]["fields"]
-                    ],
-                }
-            )
-        return clean_data
+                        "author": None
+                        if "publisher" not in dataset["dataset"]["metas"]["default"]
+                        else dataset["dataset"]["metas"]["default"]["publisher"],
+                        "licence": None
+                        if "licence" not in dataset["dataset"]["metas"]["default"]
+                        else dataset["dataset"]["metas"]["default"]["licence"],
+                        "frequency": None
+                        if "accrualperiodicity"
+                        not in dataset["dataset"]["metas"]["default"]["dcat"]
+                        else self.__check_frequency(
+                            dataset["dataset"]["metas"]["default"]["dcat"][
+                                "accrualperiodicity"
+                            ]
+                        ),
+                        "geographic_hold": None
+                        if "territory" not in dataset["dataset"]["metas"]["default"]
+                        else dataset["dataset"]["metas"]["default"]["territory"],
+                        "metadata": {
+                            "keywords": None
+                            if dataset["dataset"]["metas"]["default"]["keyword"] is None
+                            else [
+                                keyword
+                                for keyword in dataset["dataset"]["metas"]["default"][
+                                    "keyword"
+                                ]
+                            ],
+                            "description": None
+                            if dataset["dataset"]["metas"]["default"]["description"]
+                            is None
+                            else f'{remove_xml_tags(dataset["dataset"]["metas"]["default"]["description"])}',
+                            "groups": [],
+                        },
+                        "columns": [
+                            {"name": annotation["name"], "type": annotation["type"]}
+                            for annotation in dataset["dataset"]["fields"]
+                        ],
+                    }
+                )
+            return clean_data
+        except HTTPError:
+            return []
 
     async def __get_data(self) -> dict:
         """
         Private async method to get all the datasets from the DataSud API
         :return: All datasets and their count
         """
-        dataset_list = self.get_dataset_list()
+        dataset_list = self.get_dataset_list(99)
         results = await asyncio.gather(
             *map(self.get_dataset_details, await dataset_list)
         )
