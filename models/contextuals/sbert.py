c743492a14a437b70f0ed333618333a370176fda
Elie Antoine <elie.antoine@etu.univ-amu.fr>
2022-01-06 23:29:46 +0100

    Add a contextual model using the SentenceTransformers library.
    Separate the extraction of desired tags for the concatenation into a string to use into the models in  filter_data in models_tools.py .

---
+++ models/contextuals/sbert.py
@@ -0,0 +1,138 @@
+from typing import List, Any, Union, Dict, Tuple
+from numpy import ndarray
+from sentence_transformers import SentenceTransformer, util
+import torch
+import pickle
+from torch import Tensor
+from models.models_tools import filter_data
+from sklearn.cluster import AgglomerativeClustering
+import sklearn.cluster
+
+
+class ContextualEmbeddingModel:
+    def __init__(self, file_path: str = "", model: SentenceTransformer = None):
+        self.device = None
+        self.file_path: str = file_path
+        self.configuration: List[str] = []
+        self.model: SentenceTransformer = model
+        self.transformed_data_ids: List[str] = []
+        self.transformed_data: List[str] = []
+        self.embedding: Union[List[Tensor], ndarray, Tensor] = []
+
+    def set_file_path(self, file_path: str):
+        """
+        :param file_path: Set the current datasets file used for the embedding !reset the others parameters!
+        """
+        self.file_path = file_path
+
+    def set_model(self, model: str):
+        """
+        :param model: Set the current model used for the embedding !reset the others parameters!
+        """
+        self.model = model
+
+    def compute_embedding(
+        self,
+        tags_filters=None,
+        convert_to_tensor: bool = True,
+        convert_to_numpy: bool = False,
+        show_progress_bar: bool = True,
+        device: str = "cpu",
+        random_data: int = None,
+    ) -> Any:
+        """
+        :param device: Which torch.device to use for the computation (from SentenceTransformer)
+        :param show_progress_bar: Output a progress bar when encode sentences (from SentenceTransformer)
+        :param convert_to_numpy: If true, you get one large tensor as return. Overwrites any setting from convert_to_numpy (from SentenceTransformer)
+        :param convert_to_tensor: If true, you get one large tensor as return. Overwrites any setting from convert_to_numpy (from SentenceTransformer)
+        :param tags_filters: List of tags to include in the tf_idf representation default to
+        ["dataset_name", "keywords", "description", "groups"]
+        :param random_data: Number of random data picked from the transformed datas
+        """
+        self.device = device
+        if tags_filters is None:
+            tags_filters = ["dataset_name", "keywords", "description"]
+
+        self.configuration = tags_filters
+        filtered_data = filter_data(self.file_path, tags_filters, random_data)
+        self.transformed_data = list(filtered_data.values())
+        self.transformed_data_ids = list(filtered_data.keys())
+        self.embedding = self.model.encode(
+            self.transformed_data,
+            show_progress_bar=show_progress_bar,
+            convert_to_tensor=convert_to_tensor,
+            convert_to_numpy=convert_to_numpy,
+            device=device,
+        )
+
+    def save_embedding(self, name: str):
+        """
+        Save the embedding as a .pkl
+        :param name:
+        :return:
+        """
+        with open(f"{name}.pkl", "wb") as fo:
+            pickle.dump(
+                {
+                    "transformed_data": self.transformed_data,
+                    "transformed_data_ids": self.transformed_data_ids,
+                    "embeddings": self.embedding,
+                },
+                fo,
+            )
+
+    def load_embedding(self, name: str):
+        """
+        Load a .pkl embedding
+        :param name:
+        :return:
+        """
+        with open(f"{name}.pkl", "rb") as fi:
+            pickle_file = pickle.load(fi)
+            self.transformed_data = pickle_file["transformed_data"]
+            self.transformed_data_ids = pickle_file["transformed_data_ids"]
+            self.embedding = pickle_file["embeddings"]
+
+    def kmean_clustering(
+        self,
+        clustering_model: sklearn.cluster = AgglomerativeClustering(n_clusters=10),
+    ) -> Dict:
+        """
+        Compute cluster given a skelarn clustering model
+        :param clustering_model: A cluster model from sklearn.cluster
+        :return: A dict of clustered datas from the actual embedding
+        """
+        normalized_embedding = self.embedding / torch.linalg.norm(
+            self.embedding, keepdims=True
+        )
+        normalized_embedding = normalized_embedding.cpu().numpy()
+        clustering_model.fit(normalized_embedding)
+        cluster_assignment = clustering_model.labels_
+
+        clustered_sentences = {}
+        for sentence_id, cluster_id in enumerate(cluster_assignment):
+            clustered_sentences[self.transformed_data_ids[sentence_id]] = cluster_id
+
+        return clustered_sentences
+
+    def cosine_similarity(self, query: str, top_k: int = 5) -> Tuple[Any, Any]:
+        """
+        Compute the cosine similarity for a given query and return the @top_k higher similarities
+        :param query: Query
+        :param top_k: Number of document to return
+        :return: Lists of scores and ids of the @top_k higher similarities
+        """
+        top_k = min(top_k, len(self.transformed_data))
+        query_embedding = self.model.encode(
+            query, convert_to_tensor=True, device=self.device
+        )
+        cos_scores = util.cos_sim(query_embedding, self.embedding)[0]
+
+        top_results = torch.topk(cos_scores, k=top_k + 1)
+        scores = []
+        idxs = []
+        for score, idx in zip(top_results[0], top_results[1]):
+            scores.append(score.item())
+            idxs.append(idx.item())
+
+        return scores, idxs
---
+++ models/models_tools.py
@@ -0,0 +1,55 @@
+import json
+import random
+from typing import Dict, Generator, List
+
+
+def _flatten_dict_gen(d: Dict) -> Generator:
+    for k, v in d.items():
+        new_key = k
+        if isinstance(v, Dict):
+            yield from _flatten_dict(v).items()
+        else:
+            yield new_key, v
+
+
+def _flatten_dict(d: Dict) -> Dict:
+    return dict(_flatten_dict_gen(d))
+
+
+def filter_data(
+    file_path: str, tags_filters: List[str], random_data: int = None
+) -> Dict[str, str]:
+    """
+    Filter the datas to the original dataset to string for the tfidf vectorizer
+    :parameter file_path: The path of the .json datasets file
+    :parameter tags_filters: List of tags to include in the transformed datas
+    :param random_data: Number of random data picked from the transformed datas
+    :return Dict of {"id_data" : filtered_data}
+    """
+
+    with open(file_path, encoding="utf8") as f:
+        datas = json.load(f)
+
+    datasets = datas["datasets"]
+    tags = {tag: "" for tag in tags_filters}
+    transformed_data = {}
+
+    for d in datasets:
+        flatten_d = _flatten_dict(d)
+
+        for tag in tags:
+            if type(flatten_d[tag]) is list:
+                tags[tag] = " ".join(flatten_d[tag])
+            else:
+                tags[tag] = flatten_d[tag]
+
+        transformed_data[flatten_d["dataset_name"]] = " ".join(
+            filter(None, tags.values())
+        )
+
+    if random_data is not None:
+        random.seed(42)
+        dict_key = random.sample(list(transformed_data), random_data)
+        transformed_data = {k: transformed_data[k] for k in dict_key}
+
+    return transformed_data
